8c31acc0ead09ca7f88e62614c7541af3873ac35
Isme <lukecho@cherricorp.com>
2022-11-03 18:12:08 +0800

    1. Update SDK to version 2.16.2

--- Objc/JKOPAY/JKOPayExample/JKOPayExample/TPDirect.framework/Versions/A/Headers/TPDPaymentItem.h
+++ Objc/JKOPAY/JKOPayExample/JKOPayExample/TPDirect.framework/Versions/A/Headers/TPDPaymentItem.h
@@ -1,7 +1,6 @@
 //
 //  TPDPaymentItem.h
 //
-//  TPDirect iOS SDK - v2.15.0
 //  Copyright © 2017 Cherri Tech, Inc. All rights reserved.
 //
 
--- Objc/JKOPAY/JKOPayExample/JKOPayExample/TPDirect.framework/Versions/A/Headers/TPDTransactionResult.h
+++ Objc/JKOPAY/JKOPayExample/JKOPayExample/TPDirect.framework/Versions/A/Headers/TPDTransactionResult.h
@@ -1,7 +1,6 @@
 //
 //  TPDTransactionResult.h
 //
-//  TPDirect iOS SDK - v2.15.0
 //  Copyright © 2017 Cherri Tech, Inc. All rights reserved.
 //
 
